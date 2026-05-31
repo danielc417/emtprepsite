25a33ba4da73b887a8c73309b81a90dbb989186a
danielc417 <cdamerica417@gmail.com>
2026-05-31 13:53:38 -0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,17 @@
-globalThis.firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVYRyVb_BB68YRSwLIRoGHXuEL-Nu5yX0",
+  authDomain: "emt-prep-site.firebaseapp.com",
+  projectId: "emt-prep-site",
+  storageBucket: "emt-prep-site.firebasestorage.app",
+  messagingSenderId: "427775670036",
+  appId: "1:427775670036:web:3cacc1f8ef4547f082b401",
+  measurementId: "G-6ZHH7LCET2"
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
